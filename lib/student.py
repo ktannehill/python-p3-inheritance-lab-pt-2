c4a736ab9be83b74a90ed105fc92bc3a36163abd
Kat Tannehill <katetannehill@gmail.com>
2023-10-26 11:16:42 -0600

    Correct lab

--- lib/student.py
+++ lib/student.py
@@ -9,17 +9,17 @@ class Student:
 class ChattyStudent(Student):
     
     def hello(self):
-        print("Hey there! I'm so excited to learn stuff.")
+        super().hello()
         print("How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...")
 
     def raise_hand(self):
-        print("Pick me!")
-        print("Pick me!")
-        print("Pick me!")
-        print("Pick me!")
-        print("Pick me!")
-        print("Pick me!")
-        print("Pick me!")
-        print("Pick me!")
-        print("Pick me!")
-        print("Pick me!")
+        super().raise_hand()
+        super().raise_hand()
+        super().raise_hand()
+        super().raise_hand()
+        super().raise_hand()
+        super().raise_hand()
+        super().raise_hand()
+        super().raise_hand()
+        super().raise_hand()
+        super().raise_hand()
